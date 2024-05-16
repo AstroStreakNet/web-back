3b1813b2456e4cd0383501c9b8843ba09ebd4cbd
Darragh-Hazell <90960999+Darragh-Hazell@users.noreply.github.com>
2024-05-16 10:32:52 +1000

    cli request,  maybe work

--- controllers/cli_requests.go
+++ controllers/cli_requests.go
@@ -4,34 +4,34 @@ package controllers
 
 import (
 	"fmt"
-	"net/http"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-// understand cli requests
-func HandleBrowseRequest(c *gin.Context) {
-    // parse JSON request body
-    var criteria map[string]interface{}
-    if err := c.BindJSON(&criteria); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
-        return
-    }
-
-    // extract criterias
-    contains := criteria["contains"].(string)
-    notContains := criteria["notContains"].(string)
-    date := criteria["date"].(string)
-    count := criteria["count"].(int)
-    trainable := criteria["trainable"].(*bool)
-
-    // process browse criteria
-
-    // only for testing
-    fmt.Printf("[request] contains=%s, !contains=%s, date=%s, num=%d, train=",
-		contains, notContains, date, count)
-    fmt.Println(trainable)
-
-    // send response
-    c.JSON(http.StatusOK, gin.H{"message": "request received"})
+type BrowseCriteria struct {
+	Contains    string `json:"contains"`
+	NotContains string `json:"notContains"`
+	Date        string `json:"date"`
+	Count       int    `json:"count"`
+	Trainable   int    `json:"trainable"`
 }
 
+// HandleBrowseRequest understand cli requests
+func HandleBrowseRequest(c *gin.Context) {
+	// parse JSON request body
+	var criteria = BrowseCriteria{}
+
+	if err := c.BindJSON(&criteria); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+		return
+	}
+
+	// process browse criteria
+
+	// only for testing
+	fmt.Printf("[request] contains=%v, !contains=%v, date=%v, num=%v, train=%v",
+		criteria.Contains, criteria.NotContains, criteria.Date, criteria.Count, criteria.Trainable)
+
+	// send response
+	c.JSON(http.StatusOK, gin.H{"message": "request received"})
+}
